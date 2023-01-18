5a3714c67717e4b563c5ec98d4ac2356159b1091
jvandaal <jonas.vandaal@outlook.be>
2023-01-18 16:55:22 +0100

    feat: initial

---
+++ src/Be.Vlaanderen.Basisregisters.DockerUtilities/DockerComposer.cs
@@ -0,0 +1,20 @@
+﻿namespace Be.Vlaanderen.Basisregisters.DockerUtilities
+{
+    using Ductus.FluentDocker.Builders;
+    using Ductus.FluentDocker.Services;
+
+    public static class DockerComposer
+    {
+        public static ICompositeService Compose(string dockerComposeFilePath, string serviceName)
+        {
+            return new Builder()
+                .UseContainer()
+                .UseCompose()
+                .ServiceName(serviceName)
+                .FromFile(dockerComposeFilePath)
+                .RemoveOrphans()
+                .Build()
+                .Start();
+        }
+    }
+}
