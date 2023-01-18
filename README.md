a7f2cdfd86fd228c2a6c217288d5891bc03d8736
jvandaal <jonas.vandaal@outlook.be>
2023-01-18 16:55:22 +0100

    feat: initial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # basisregisters-docker-utilities
+
+Docker-compose utilities for C#. 
+
+    var dockerComposeFilePath = Path.Combine(Directory.GetCurrentDirectory(), "postgres_test.yml");
+    var serviceName = "dockerutilities-postgres-sample";
+
+    var compositeService = DockerComposer.Compose(dockerComposeFilePath, serviceName);
---
+++ samples/DockerUtilities.Postgresql.Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Be.Vlaanderen.Basisregisters.DockerUtilities;
+
+var dockerComposeFilePath = Path.Combine(Directory.GetCurrentDirectory(), "postgres_test.yml");
+var serviceName = "dockerutilities-postgres-sample";
+
+Console.WriteLine("Performing docker-compose up for postgres_test.yml");
+
+var compositeService = DockerComposer.Compose(dockerComposeFilePath, serviceName);
+
+Console.WriteLine("Container is running");
+
+Console.WriteLine("Hit any key to perform docker-compose down");
+Console.ReadKey();
+
+compositeService.Stop();
+
