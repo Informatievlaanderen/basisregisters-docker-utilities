e9f63050d3ccf4a9b1276e6e96f8f3345f5868d4
jvandaal <jonas.vandaal@outlook.be>
2024-03-12 15:01:31 +0100

    feat: move to dotnet 8.0.2
    move to dotnet 8.0.2
    
    BREAKING CHANGE: move to dotnet 8.0.2

---
+++ src/Be.Vlaanderen.Basisregisters.DockerUtilities/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Basisregisters Vlaanderen")]
+[assembly: AssemblyDescription("Docker utilities for C#.")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("d69c7059-d593-4f25-b386-8bfc7dea5f6b")]
