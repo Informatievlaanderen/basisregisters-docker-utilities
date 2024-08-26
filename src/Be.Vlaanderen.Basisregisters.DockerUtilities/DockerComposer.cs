1d937595ed30646e104564cfa912428c8e055a68
jvandaal <jonas.vandaal@outlook.be>
2024-08-26 16:07:33 +0200

    feat: support docker compose V2
    BREAKING CHANGE: docker compose v2 is now the default

--- src/Be.Vlaanderen.Basisregisters.DockerUtilities/DockerComposer.cs
+++ src/Be.Vlaanderen.Basisregisters.DockerUtilities/DockerComposer.cs
@@ -1,15 +1,20 @@
 ﻿namespace Be.Vlaanderen.Basisregisters.DockerUtilities
 {
     using Ductus.FluentDocker.Builders;
+    using Ductus.FluentDocker.Model.Compose;
     using Ductus.FluentDocker.Services;
 
     public static class DockerComposer
     {
-        public static ICompositeService Compose(string dockerComposeFilePath, string serviceName)
+        public static ICompositeService Compose(
+            string dockerComposeFilePath,
+            string serviceName,
+            ComposeVersion dockerComposeVersion = ComposeVersion.V2)
         {
             return new Builder()
                 .UseContainer()
                 .UseCompose()
+                .AssumeComposeVersion(dockerComposeVersion)
                 .ServiceName(serviceName)
                 .FromFile(dockerComposeFilePath)
                 .RemoveOrphans()
